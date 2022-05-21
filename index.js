1b69dbf269675caa6e8c8050c5bb0aa012252fc5
Robala <robala998@gmail.com>
2022-05-20 23:41:48 -0500

    trying my best to add but its broken

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 const consTable = require("console.table")
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+
+
+function addEmployee() { 
+    inquirer.prompt([
+        {
+          name: "first",
+          type: "input",
+          message: "Enter the first name "
+        },
+        {
+          name: "last",
+          type: "input",
+          message: "Enter the last name "
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "What is the role? ",
+          choices: selectRole()
+        },
+        {
+            name: "choice",
+            type: "list",
+            message: "Whats the managers name?",
+            choices: selectManager()
+        }
+    ]).then(function (val) {
+      var roleId = selectRole().indexOf(val.role) + 1
+      var managerId = selectManager().indexOf(val.choice) + 1
+      connection.query("INSERT INTO employee SET ?", 
+      {
+          first_name: val.firstName,
+          last_name: val.lastName,
+          manager_id: managerId,
+          role_id: roleId
+          
+      }, function(err){
+          if (err) throw err
+          console.table(val)
+          startPrompt()
+      })
+
+  })
+}
